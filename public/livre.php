e010cbdc437f9bbef1c36c2b406b136119f8ec8d
aliii390 <alli.said.errahmani@gmail.com>
2025-02-18 10:14:46 +0100

    ADD: refacto page livre

--- public/livre.php
+++ public/livre.php
@@ -18,18 +18,18 @@ $livre = $postLivreRepo->findById($livreId);
 </head>
 <body class="bg-[#FDEDD5] flex flex-col min-h-screen">
 
-<main class="p-3 lg:mt-36">
-  <section class="flex flex-col justify-center items-center lg:flex-row lg:gap-10 lg:items-start">
+<main class="p-3 lg:mt-52 ">
+  <section class="flex flex-col justify-center items-center lg:flex-row  lg:gap-10 lg:items-center lg:justify-center">
     <!-- Image à gauche -->
     <img src="<?php echo $livre['img_path']; ?>" alt="Livre" class="w-32 sm:w-40 md:w-48 lg:w-64 lg:mr-10">
     
     <!-- Texte et bouton à droite -->
-    <article class="flex flex-col items-center lg:items-start gap-6 lg:w-1/2">
+    <article class="flex flex-col items-center lg:items-center  gap-6 ">
       <h1 class="font-principale text-xl md:text-2xl lg:text-3xl text-center lg:text-left">
         <?php echo $livre['titre']; ?>
       </h1>
       <h3 class="font-principale text-xl md:text-2xl lg:text-3xl text-[#F5702B]">
-        € <?php echo $livre['prix']; ?>
+        <?php echo $livre['prix']; ?> € 
       </h3>
       <p class="w-56 sm:w-64 md:w-80 lg:w-full font-principale text-justify">
         <span class="font-bold text-black">Résumé: </span>
@@ -44,8 +44,7 @@ $livre = $postLivreRepo->findById($livreId);
   </section>
 </main>
 
-<footer class="bg-[#2A3D37] flex items-center lg:flex-row justify-between p-4 mt-auto">
-  <!-- Votre contenu de pied de page -->
-</footer>
+
+
 </body>
 </html>
